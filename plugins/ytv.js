2297d97050cd0d49e188e3c6cb2ed66973094e2a
fafhghg1 <94165730+fafhghg1@users.noreply.github.com>
2022-02-20 15:16:55 +0700

    Update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -1,4 +1,4 @@
-let limit = 100
+let limit = 150
 let fetch = require('node-fetch')
 const { servers, ytv } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
@@ -10,7 +10,7 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
 *Title:* ${title}
 *Filesize:* ${filesizeF}
-*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
+*${isLimit ? 'file melebihi 150 mb jadi pakai ': ''}Link:* ${dl_link}
 `.trim(), m)
   let _thumb = {}
   try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
